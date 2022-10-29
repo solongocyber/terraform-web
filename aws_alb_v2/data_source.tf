7d85138592e902177c283c4bd684eed5e82b1822
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-29 23:18:43 +0000

    alb_v2

--- aws_alb_v2/data_source.tf
+++ aws_alb_v2/data_source.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc_v3" {
   backend = "s3"
   config = {
     bucket = "aws-session-may2022-solongo"
-    key    = "vpc_v3/terraform.tfstate"
+    key    = "solongo/vpc_v3/terraform.tfstate"
     region = "us-east-2"
   }
 }
